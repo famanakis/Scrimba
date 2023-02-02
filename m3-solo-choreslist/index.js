8b90ac3086055287cc234453ee9ee585645c7cb6
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-01 18:12:28 -0700

    update js
    js is in working condition for basic requirements of the project

--- m3-solo-choreslist/index.js
+++ m3-solo-choreslist/index.js
@@ -7,18 +7,52 @@ const modal = document.getElementById('modal')
 const btnClose = document.getElementById('close-btn')
 
 //Global Variables
-choresArr = []
+// choresArr = []
+window.localStorage.length > 0 ?
+    choresArr = JSON.parse(window.localStorage.getItem('chore')) :
+    choresArr = []
 
+//Event Listeners
+btnAdd.addEventListener('click', ()=>{
+    inputEl.value.length === 0 ? inputEl.placeholder = 'Do dishes' : addChore()
+})
 
+btnRemove.addEventListener('click', ()=> {
+    clearChorelist()
+})
 
-
+btnClose.addEventListener('click', () => {
+    modal.style.visibility = 'hidden'
+})
 
 
 //Functions
+renderChores()
+
+function renderChores() {
+    list.innerHTML = '' 
+    choresArr.forEach(i => {
+        list.innerHTML += `<div class="chore center">${i}</div>`
+    })
+}
+
+function addChore() {
+    if(!choresArr.includes(inputEl.value)){
+        choresArr.push(inputEl.value)
+        window.localStorage.setItem('chore', JSON.stringify(choresArr))
+    }
+    renderChores()
+}
+
+function clearChorelist() {
+    list.innerHTML = ''
+    inputEl.placeholder = 'Do dishes'
+    inputEl.value = ''
+    choresArr = []
+    window.localStorage.clear()
+}
+
 function showModal() {
     modal.style.visibility = 'visible'
 }
 
-btnClose.addEventListener('click', () => {
-        modal.style.visibility = 'hidden'
-    })

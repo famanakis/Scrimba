47f62b648174fd16714ebebed1f057bc1156bc42
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-06 23:01:06 -0700

    update oldagram
    removed negative margins from css, made header sticky.  in js added a decrement to the likes value if isLiked is true, and increment to the likes value if isLiked is false.

--- m4-solo-oldagram/index.js
+++ m4-solo-oldagram/index.js
@@ -10,7 +10,8 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        isLiked: false
     },
     {
         name: "Gustave Courbet",
@@ -19,7 +20,8 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        isLiked: false
     },
         {
         name: "Joseph Ducreux",
@@ -28,15 +30,17 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked: false
     }
 ]
 
 //Functions
-renderPage()
+renderPosts()
 
-function renderPage() {
+function renderPosts() {
     let html = ''
+
     for (let i = 0; i < posts.length; i++) { 
         html += `
             <section class="title-section">
@@ -54,8 +58,8 @@ function renderPage() {
             <section class="post-info">
                 <div class="icon-div">
                     <button class="btn" onClick="addLikesToPage(${i})">
-                        <img class="heart" src="images/icon-heart.png"></button>
-                    <button class="btn"><img class="comment"         src="images/icon-comment.png"></button>
+                    <i id="${posts[i].username}" class="fa-regular fa-heart"></i></button>
+                    <button class="btn"><img class="comment" src="images/icon-comment.png"></button>
                     <button class="btn"><img class="dm" src="images/icon-dm.png"></button>
                 </div>
                 <div>
@@ -63,16 +67,25 @@ function renderPage() {
                 <p><span class="bold likesBtn">${posts[i].username}</span> ${posts[i].comment}</p>
                 </div>
             </section>`
+
+            
     }
     postContainerEl.innerHTML = html   
 }
 
-
-//function to add likes to user - Increase like count by one when function is called
+//function to add likes to user - 
+    // Increase by one if isLikes is false, decrease by one if isLiked is true
 function addLikesToPage(i) {
-        let likesCount = posts[i].likes
-        likesCount++
-        posts[i].likes = likesCount
-        document.getElementById("currentLikes").textContent = likesCount
-        renderPage()
-}
+    let likesCount = posts[i].likes
+        if(!posts[i].isLiked) {        
+            likesCount++
+            posts[i].likes = likesCount
+            renderPosts()  
+            posts[i].isLiked = true
+        } else {
+            likesCount--
+            posts[i].likes = likesCount
+            renderPosts()  
+            posts[i].isLiked = false        
+        }
+    }

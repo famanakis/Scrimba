b0b910462b40f3fe93c2fc10b6715ce567d8ff4c
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-08 13:04:32 -0700

    update

--- m5-solo-invoice-creator/index.js
+++ m5-solo-invoice-creator/index.js
@@ -1,179 +1,105 @@
-//Accessing the DOM//
-const invoiceContainer = document.getElementById("services-container")
-const taskEl = document.getElementById("taskEl")
-const totalEl = document.getElementById("totalEl")
+//Accessing the DOM
+const task = document.getElementById('input-task')
+const price = document.getElementById('input-price')
+const btnAdd = document.getElementById('btn-add-task')
+const invoicesEl = document.getElementById("tasks-container")
+const remove = document.getElementById('remove')
 const termsEl = document.getElementById("terms")
-const invoiceTotal = document.getElementById("invoiceTotal")
-const btnCar = document.getElementById("btn-car")
-const btnLawn = document.getElementById("btn-lawn")
-const btnWeeds = document.getElementById("btn-weeds")
+const invoiceTotalEl = document.getElementById("invoiceTotal")
 const btnSendInvoice = document.getElementById("btn-send")
 
-const btnEls = document.querySelectorAll('button')
+let tasksArr = []
 
-document.body.addEventListener('click', (e) => {
-    let task = e.target.dataset
-    console.log(task.value)
+//Event Listeners
+btnAdd.addEventListener('click', ()=> {
+    addTask()
+    renderTasksArr()
+    renderInvoiceTotal()
 })
 
-
-//Global
-const serviceArray = []
-const totalArray = []
-
-//Onclick Wash Car Button does this...   
-btnCar.addEventListener("click", ()=> {
-    serviceArray.push("Wash Car")
-    totalArray.push(10)  
-    invoiceContainer.innerHTML += `
-        <div class="flex-container" id="car">
-        <h2 id="taskEl">Wash Car<span class="remove less-margin" onclick="removeCar()">Remove</span></h2>
-        <h2 id="totalEl" class="less-margin"><span class="less">$</span> 10</h2>
-        </div>`
-    termsEl.innerHTML = `
-        <p>We accept cash, credit card or Paypal</p>`
-    renderTotalAmount()
-    btnCar.disabled = true 
-})
-
-//Onclick Mow Lawn Button does this...
-btnLawn.addEventListener("click", ()=> {
-    serviceArray.push("Mow Lawn")
-    totalArray.push(20)  
-    invoiceContainer.innerHTML += `
-        <div class="flex-container" id="mow">
-        <h2 id="taskEl">Mow Lawn<span class="remove" onclick="removeLawn()">Remove</span></h2>
-        <h2 id="totalEl" class="less-margin"><span class="less">$</span> 20</h2>
-        </div>`
-    termsEl.innerHTML = `
-        <p>We accept cash, credit card or Paypal</p>`
-    renderTotalAmount()
-    btnLawn.disabled = true
-})
-
-//Onclick Pull Weeds Button does this... 
-btnWeeds.addEventListener("click", ()=> {
-    totalArray.push(30) 
-    serviceArray.push("Pull Weeds")
-    invoiceContainer.innerHTML += `
-        <div class="flex-container" id="pull">
-        <h2 id="taskEl">Pull Weeds<span class="remove" onclick="removeWeeds()">Remove</span></h2>
-        <h2 id="totalEl" class="less-margin"><span class="less">$</span> 30</h2>
-        </div>`
-    termsEl.innerHTML = `
-        <p>We accept cash, credit card or Paypal</p>`
-    renderTotalAmount()
-    btnWeeds.disabled = true
-})
-
-//Render and/or update Invoice Total to DOM
-function renderTotalAmount() {
-    invoice = totalArray.reduce((a, b)=> a + b)
-    invoiceTotal.innerText = "$ " + invoice
-    }
-
-
-//Send Button - currently reloads the page
 btnSendInvoice.addEventListener("click", ()=> {
-        invoiceContainer.innerHTML = ''
-        invoiceTotal.innerText = "$ 0"    
+    tasksArr = []
+    task.value = ''
+    price.value = 10
+    invoicesEl.innerHTML = ''
+    invoiceTotalEl.innerText = "$ 0"    
+    termsEl.innerHTML = ''
+})
+
+//Functions
+function addTask() {
+    let name = task.value 
+    let cost = price.value 
+    name.length === 0 ? tasksArr :
+        tasksArr.push({name: name, cost: cost, isListed: true, uuid: uuid()})
+    task.value = ''
+    price.value = 10
+}
+
+renderTasksArr()
+function renderTasksArr() {
+    let html = ''
+    tasksArr.forEach((task) => {
+        html += `
+            <div class="flex-container">
+            <h2>${task.name}<span class="remove" id="remove" data-uuid="${task.uuid}">Remove</span></h2>
+            <h2><span>$</span> ${task.cost}</h2>
+            </div>`
+        })
+    invoicesEl.innerHTML = html
+    tasksArr.length > 0 ? termsEl.innerHTML = 
+        `<p class="terms">We accept cash, credit card, or Paypal</p>` :
         termsEl.innerHTML = ''
-        document.querySelectorAll('button').forEach(button => button.disabled = false)
+}
+
+function renderInvoiceTotal() {
+    const totalsArr = []
+    tasksArr.forEach((task) => {
+        totalsArr.push(parseInt(task.cost))
+        })
+    let invoiceTotal = 0
+    totalsArr.length > 0 ? invoiceTotal = totalsArr.reduce(function(a, b){return a + b}) : invoiceTotal
+    invoiceTotalEl.innerText = "$ " + invoiceTotal
+}
+
+function uuid() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c=>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        )
+}
+
+function removeEl(e) {
+    e.target.dataset.uuid
+    const filteredArr = tasksArr.filter(task => {
+        return task.uuid != e.target.dataset.uuid
     })
-
-
-//Functions to Remove items from DOM:
-//     Also Re-enables Service Button, Updates Invoice Total and Updates the Arrays//
-
-//Remove Car Wash from the Dom and the Arrays
-function removeCar() {
-    const removeCarEl = document.getElementById("car")
-    removeCarEl.remove()
-    // invoice -= 10
-    renderTotalAmount()
-    btnCar.disabled = false
-    for (let i=0; i<totalArray.length; i++) {
-        if (totalArray[i] === 10) {
-            totalArray.splice(i,1)
-        }
-    }
-    for (let k=0; k<serviceArray.length; k++) {
-        if (serviceArray[k] === "Wash Car") {
-            serviceArray.splice(k,1)
-        }
-        if (serviceArray.length === 0) {
-            termsEl.innerHTML = ''
-        }
-    }   
+    tasksArr = filteredArr
+    renderTasksArr()
+    renderInvoiceTotal()
 }
 
-//Remove Mow Lawn from the Dom and the Arrays
-function removeLawn() {
-    const removeMowEl = document.getElementById("mow")
-    removeMowEl.remove()
-    // invoice -= 20;
-    renderTotalAmount()
-    btnLawn.disabled = false;
-    for (let i=0; i<totalArray.length; i++) {
-        if (totalArray[i] === 20) {
-            totalArray.splice(i,1)
+//Custom Event Listener for targeted 'span#remove'
+//this works - but I am unsure if there is a better way to do this - any advice appreciated
+addCustomEventListener('span#remove', 'click', removeEl)
+
+function addCustomEventListener(selector, event, handler) {
+    let rootElement = document.querySelector('body')
+    rootElement.addEventListener(event, function (e) {
+        let targetElement = e.target
+        while (targetElement != null) {
+            if(targetElement.matches(selector)) {
+                handler(e)
+                return
+            }
+            targetElement = targetElement.parentElement
         }
-    }
-    for (let k=0; k<serviceArray.length; k++) {
-        if (serviceArray[k] === "Mow Lawn") {
-            serviceArray.splice(k,1)
-        }
-        if (serviceArray.length === 0) {
-            termsEl.innerHTML = ''
-        }
-    }   
+    },
+    true
+    )
 }
 
-//Remove Pull Weeds from the Dom and the Arrays
-function removeWeeds() {
-    const removePullEl = document.getElementById("pull")
-    removePullEl.remove()
-    // invoice -= 30;
-    renderTotalAmount()
-    btnWeeds.disabled = false
-    for (let i=0; i<totalArray.length; i++) {
-        if (totalArray[i] === 30) {
-            totalArray.splice(i,1)
-        }
-    }
-    for (let k=0; k<serviceArray.length; k++) {
-        if (serviceArray[k] === "Pull Weeds") {
-            serviceArray.splice(k,1)
-        }
-        if (serviceArray.length === 0) {
-            termsEl.innerHTML = ''
-        }
-    }    
-}
- 
+
+
+
  
 
-tasks = [
-    {   
-        id: 1,
-        name: 'Wash Car',
-        cost: 10,
-        isListed: false
-    },
-    {   
-        id: 2,
-        name: 'Mow Lawn',
-        cost: 20,
-        isListed: false
-    },
-    {
-        id: 3,
-        name: 'Pull Weeds',
-        cost: 30,
-        isListed: false
-    }
-]
-
-// inputEl.value is name, number.value is cost, isListed defaults to true 
-tasks.push({id: tasks.length + 1, name: 'me', cost: 40, isListed: true})
-console.log(tasks)

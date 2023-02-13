1f211aa5f549a734da62ac53df0ef5fb69afdc88
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-13 09:42:52 -0700

    update

--- m5-solo-invoice-creator/index.js
+++ m5-solo-invoice-creator/index.js
@@ -8,23 +8,7 @@ const termsEl = document.getElementById("terms")
 const invoiceTotalEl = document.getElementById("invoiceTotal")
 const btnSendInvoice = document.getElementById("btn-send")
 
-let tasksArr = []
-
-//Event Listeners
-btnAdd.addEventListener('click', ()=> {
-    addTask()
-    renderTasksArr()
-    renderInvoiceTotal()
-})
-
-btnSendInvoice.addEventListener("click", ()=> {
-    tasksArr = []
-    task.value = ''
-    price.value = 10
-    invoicesEl.innerHTML = ''
-    invoiceTotalEl.innerText = "$ 0"    
-    termsEl.innerHTML = ''
-})
+const tasksArr = []
 
 //Functions
 function addTask() {
@@ -38,15 +22,13 @@ function addTask() {
 
 renderTasksArr()
 function renderTasksArr() {
-    let html = ''
-    tasksArr.forEach((task) => {
-        html += `
-            <div class="flex-container">
+    // let html = ''
+    invoicesEl.innerHTML = tasksArr.map((task) => (
+            `<div class="flex-container">
             <h2>${task.name}<span class="remove" id="remove" data-uuid="${task.uuid}">Remove</span></h2>
             <h2><span>$</span> ${task.cost}</h2>
             </div>`
-        })
-    invoicesEl.innerHTML = html
+        )).join(' ')
     tasksArr.length > 0 ? termsEl.innerHTML = 
         `<p class="terms">We accept cash, credit card, or Paypal</p>` :
         termsEl.innerHTML = ''
@@ -69,37 +51,74 @@ function uuid() {
 }
 
 function removeEl(e) {
-    e.target.dataset.uuid
-    const filteredArr = tasksArr.filter(task => {
-        return task.uuid != e.target.dataset.uuid
-    })
-    tasksArr = filteredArr
+    //Old way done with Filter
+    // e.target.dataset.uuid
+    // const filteredArr = tasksArr.filter(task => {
+    //     return task.uuid != e.target.dataset.uuid
+    // })
+    // tasksArr = filteredArr
+    //create const that finds the index of the element ou click to remove
+    const taskIndex = tasksArr.indexOf(
+        tasksArr.find(task => task.uuid === e.target.dataset.uuid)
+    )
+    //splice the taskIndex element out of the tasksArr
+    tasksArr.splice(taskIndex, 1)
     renderTasksArr()
     renderInvoiceTotal()
 }
 
-//Custom Event Listener for targeted 'span#remove'
-//this works - but I am unsure if there is a better way to do this - any advice appreciated
-addCustomEventListener('span#remove', 'click', removeEl)
-
-function addCustomEventListener(selector, event, handler) {
-    let rootElement = document.querySelector('body')
-    rootElement.addEventListener(event, function (e) {
-        let targetElement = e.target
-        while (targetElement != null) {
-            if(targetElement.matches(selector)) {
-                handler(e)
-                return
-            }
-            targetElement = targetElement.parentElement
-        }
-    },
-    true
-    )
+function sendInvoice() {
+    tasksArr.length = 0
+    task.value = ''
+    price.value = 10
+    invoicesEl.innerHTML = ''
+    invoiceTotalEl.innerText = "$ 0"    
+    termsEl.innerHTML = ''
 }
 
+//Event Listeners
+document.addEventListener('click', e => {
+    e.preventDefault
+    if(e.target.id === 'btn-add-task') {
+        console.log('add')
+        addTask()
+        renderTasksArr()
+        renderInvoiceTotal()
+    }
+    if(e.target.id === 'remove') {
+        removeEl(e)
+    }
+    if(e.target.id === 'btn-send') {
+        sendInvoice()
+    }
+})
 
+//Old way done with custom and multiple event listeners
 
+// btnAdd.addEventListener('click', ()=> {
+//     addTask()
+//     renderTasksArr()
+//     renderInvoiceTotal()
+// })
+
+// btnSendInvoice.addEventListener("click", sendInvoice)
+
+// addCustomEventListener('span#remove', 'click', removeEl)
+// function addCustomEventListener(selector, event, handler) {
+//     let rootElement = document.querySelector('body')
+//     rootElement.addEventListener(event, function (e) {
+//         let targetElement = e.target
+//         while (targetElement != null) {
+//             if(targetElement.matches(selector)) {
+//                 handler(e)
+//                 return
+//             }
+//             targetElement = targetElement.parentElement
+//         }
+//     },
+//     true
+//     )
+// }
 
  
 

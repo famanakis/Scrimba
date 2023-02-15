1ef48880b1dc521225703352c7bafe8b2666c7d3
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-15 14:59:41 -0700

    commit

---
+++ m5-solo-restaraunt-ordering-app/setOrderHtml.js
@@ -0,0 +1,62 @@
+import {menuData} from "./menuData.js"
+import {orderArr} from "./index.js"
+
+//Render Line Items to the Page
+export function setOrderHtml() {
+    //Render Array of Items Ordered to Page
+    let orderHtml = '' 
+    orderArr.forEach(obj => {  
+        menuData.forEach(item => {
+            if(item.uuid === obj.uuid) {
+                orderHtml += `
+                <div class="order-subtotal flex-row">
+                <div class="flex-row">
+                    <p class="list-item">${item.name}</p>
+                    <button id="remove=item" 
+                    class="remove-btn"
+                    data-remove="${obj.id}">remove</button>
+                </div>
+                <p class="list-total">$${item.price}</p>
+                </div>`
+            }
+        })
+    })
+    //Render Meal Deal to Page
+    const entreeArr = []
+    const sideArr = []
+        orderArr.forEach((obj => {
+            menuData.forEach(item=> {
+                if(item.uuid === obj.uuid){
+                if(item.category === 'entree') {
+                    entreeArr.push(item.category) 
+                } else if(item.category === 'side') {
+                    sideArr.push(item.category)
+                }               
+            }
+        })
+    }))       
+    if(entreeArr.length >= 2 && sideArr.length >= 2) {
+        orderHtml += `
+            <div class="order-subtotal flex-row">
+                <div class="flex-row">
+                    <p class="list-item">Meal Deal Soda</p>
+                </div>
+                <p class="list-total free">FREE</p>
+            </div>
+            <div class="order-subtotal flex-row">
+            <div class="flex-row">
+                <p class="list-item">Meal Deal Soda</p>
+            </div>
+            <p class="list-total free">FREE</p>
+            </div>`
+    } else if (entreeArr.length >= 1 && sideArr.length >= 1) {
+        orderHtml += `
+            <div class="order-subtotal flex-row">
+                <div class="flex-row">
+                    <p class="list-item">Meal Deal Soda</p>
+                </div>
+                <p class="list-total free">FREE</p>
+            </div>`
+    } 
+    return orderHtml
+}
--- m5-solo-restaraunt-ordering-app/setOrderTotalHtml.js
+++ m5-solo-restaraunt-ordering-app/setOrderTotalHtml.js
@@ -1,66 +1,6 @@
 import {menuData} from "./menuData.js"
 import {orderArr} from "./index.js"
-
-
-//Render Line Items to the Page
-export function renderOrderArr() {
-    //Render Array of Items Ordered to Page
-    let orderHtml = '' 
-    orderArr.forEach(obj => {  
-        menuData.forEach(item => {
-            if(item.uuid === obj.uuid) {
-                orderHtml += `
-                <div class="order-subtotal flex-row">
-                <div class="flex-row">
-                    <p class="list-item">${item.name}</p>
-                    <button id="remove=item" 
-                    class="remove-btn"
-                    data-remove="${obj.id}">remove</button>
-                </div>
-                <p class="list-total">$${item.price}</p>
-                </div>`
-            }
-        })
-    })
-    //Meal Deal Conditional Rendering to Page
-    const entreeArr = []
-    const sideArr = []
-        orderArr.forEach((obj => {
-            menuData.forEach(item=> {
-                if(item.uuid === obj.uuid){
-                if(item.category === 'entree') {
-                    entreeArr.push(item.category) 
-                } else if(item.category === 'side') {
-                    sideArr.push(item.category)
-                }               
-            }
-        })
-    }))       
-    if(entreeArr.length >= 2 && sideArr.length >= 2) {
-        orderHtml += `
-            <div class="order-subtotal flex-row">
-                <div class="flex-row">
-                    <p class="list-item">Meal Deal Soda</p>
-                </div>
-                <p class="list-total free">FREE</p>
-            </div>
-            <div class="order-subtotal flex-row">
-            <div class="flex-row">
-                <p class="list-item">Meal Deal Soda</p>
-            </div>
-            <p class="list-total free">FREE</p>
-            </div>`
-    } else if (entreeArr.length >= 1 && sideArr.length >= 1) {
-        orderHtml += `
-            <div class="order-subtotal flex-row">
-                <div class="flex-row">
-                    <p class="list-item">Meal Deal Soda</p>
-                </div>
-                <p class="list-total free">FREE</p>
-            </div>`
-    } 
-    return orderHtml
-}
+import {setOrderHtml} from "./setOrderHtml.js"
 
 //Render Order Grand Total to Page
 export function setOrderTotalHtml() {
@@ -77,7 +17,7 @@ export function setOrderTotalHtml() {
     if(orderArr.length >=1) {
         orderTotalHtml += `
         <p class="your-order">Your order</p>   
-        ${renderOrderArr()}  
+        ${setOrderHtml()}  
         <span class="total flex-row">
             <p class="total-text">Total price:</p>
             <p class="total-price">$${sumOrderArr}</p>

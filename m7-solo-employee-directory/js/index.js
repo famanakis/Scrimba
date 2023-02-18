468f5f3a5735c5df81ecc6c54140d1faf78730f0
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-18 13:07:50 -0700

    commit

--- m7-solo-employee-directory/js/index.js
+++ m7-solo-employee-directory/js/index.js
@@ -3,22 +3,25 @@ import { employees } from "./employees.js"
 
 //Access the DOM
 const employeeCardDiv = document.getElementById('card-container')
-const category = document.getElementById('input-select-category')
+const team = document.getElementById('input-select-team')
 const searchInput = document.getElementById('input-search')
 
 
 class Employee {
     constructor(data) {
-        Object.assign(this, data)
-        
+        Object.assign(this, data)      
     }
 
-    // renderTwitter() {
-    //     if(this.social.twitter) {`<a href="https://twitter.com/"><img src="images/assets/twitter-icon.png" alt="social link"></a>`}
-    //     if(this.social.linkedin) {`<a href="https://www.linkedin.com/"><img src="images/assets/linkedIn-icon.png" alt="social link"></a>`}
-    // }
-
-
+    renderSocialIcons() {
+        if (this.social.twitter && this.social.linkedin) {
+                return `<a href="https://twitter.com/"><img src="images/assets/twitter-icon.png" alt="social link"></a>
+                        <a href="https://www.linkedin.com/"><img src="images/assets/linkedIn-icon.png" alt="social link"></a>`
+            } else if (this.social.twitter) {
+                return `<a href="https://twitter.com/"><img src="images/assets/twitter-icon.png" alt="social link"></a>`
+            } else {
+                return `<a href="https://www.linkedin.com/"><img src="images/assets/linkedIn-icon.png" alt="social link"></a>`   
+            } 
+    }
 
     renderCardHtml() {
         const { name, title, team, bio, image, social} = this
@@ -26,32 +29,17 @@ class Employee {
                     <img src="images/photos/${image}" class="employee-picture" alt="picture of jeremy">
                     <h2>${name}</h2>
                     <h3>${title}</h3>
-                    ${bio}
+                    <span class="bio">${bio}</span>
                     <span class="socials-div">
-                       
+                    ${this.renderSocialIcons()}
                     </span>   
                 </div>`
     }
-
 }
 
-const employeeCard = new Employee(employees[1])
-employeeCardDiv.innerHTML = employeeCard.renderCardHtml()
+const employeeCards = employees.map((employees) => {
+    const employeeCard = new Employee(employees)
+    return employeeCard.renderCardHtml()
+})
 
-// console.log(employeeCard.socialLinks())
-
-/* <div class="card">
-    <img src="images/photos/jeremy.jpg" class="employee-picture" alt="picture of jeremy">
-    <h2>Jeremy Durant</h2>
-    <h3>CEO</h3>
-    <p>Jeremy developed an interest in computers at an early age. He graduated with a computer science and electrical engineering degree.</p>
-    <p>He was an early founder of successful companies like BlueSpace, Eazybit, and Drifty.</p>
-    <img src="images/assets/linkedIn-icon.png" alt="social link">
-</div>  */
-
-
-
-
-
-
-
+employeeCardDiv.innerHTML = employeeCards.join('')

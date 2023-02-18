3023f180458ddc8476fff0dd98d308479a1b08af
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-17 17:42:03 -0700

    commit

--- m7-solo-employee-directory/js/index.js
+++ m7-solo-employee-directory/js/index.js
@@ -3,4 +3,8 @@ import { employees } from "./employees.js"
 
 //Access the DOM
 const employeeCardDiv = document.getElementById('card-container')
+const category = document.getElementById('input-select-category')
+const searchInput = document.getElementById('input-search')
+
+
 

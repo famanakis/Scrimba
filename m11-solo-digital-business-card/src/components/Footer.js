022a48657af3cf72699a4b23b8899bca9bd969ce
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-23 19:28:22 -0700

    commit

--- m11-solo-digital-business-card/src/components/Footer.js
+++ m11-solo-digital-business-card/src/components/Footer.js
@@ -3,10 +3,10 @@ import React from "react"
 export default function Footer() {
     return (
         <footer className="footer">
-            <a href=''><i className="fa-brands fa-square-twitter"></i></a>
-            <a href=''><i className="fa-brands fa-square-facebook"></i></a>
-            <a href=''><i className="fa-brands fa-square-instagram"></i></a>
-            <a href=''><i className="fa-brands fa-square-github"></i></a>
+            <a href='https://twitter.com/FMamanakis'><i className="fa-brands fa-square-twitter"></i></a>
+            <a href='https://www.facebook.com/farrah.mamanakis'><i className="fa-brands fa-square-facebook"></i></a>
+            <a href='https://www.instagram.com/9tfdev/'><i className="fa-brands fa-square-instagram"></i></a>
+            <a href='https://github.com/famanakis'><i className="fa-brands fa-square-github"></i></a>
         </footer>
     )
 }

4ae90bc6b435c5e5194cdb472e7998b335bf47de
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-23 23:02:57 -0700

    commit

--- m11-solo-travel-journal/src/components/Header.js
+++ m11-solo-travel-journal/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <header>
             <nav className="navbar">
                 <img src={require ("../images/world-icon.png")} alt="world icon" className="nav-logo"/>
-                <h6 className="nav-title">Farrah's Travel Journal</h6>
+                <h1 className="nav-title">Farrah's Travel Journal</h1>
             </nav>
         </header>
     )

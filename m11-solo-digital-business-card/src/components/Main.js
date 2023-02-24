52b598c48ccd6bbf0fc48ddc3049197e77e0fe42
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-23 19:27:24 -0700

    commit

--- m11-solo-digital-business-card/src/components/Main.js
+++ m11-solo-digital-business-card/src/components/Main.js
@@ -16,9 +16,9 @@ export default function Main() {
             </div>
             <div>
                 <h4>About</h4>
-                <p>I am a frontend developer interested in making attractive websites with pleasing UI design. I try to keep up with best practices and cient security needs. I enjoy learning new techology as it emerges in the ever-evolving tech/dev world. </p>
+                <p>I am a frontend developer interested in making attractive websites with pleasing UI design. I try to keep up with best practices and client security needs. I enjoy learning new technology as it emerges in the ever-evolving tech/dev world. </p>
                 <h4>Strengths</h4>
-                <p>Attention to Detail. Knowledge of Agile/Scrum. Working with ASD. College Minor in Business Management. Accounts Management Experience.</p>
+                <p>Attention to Detail. Knowledge of Agile/Scrum. Degrees/Experience in Education, Business Management, Fine Arts Technical Production, Accounts Management and working with individuals on the Autistic Spectrum.</p>
                 <h4>Interests</h4>
                 <p>Food expert. Classical piano. KDrama fanatic. Dance Cardio enthusiast. Beach and Lakefront fan.</p>
             </div>

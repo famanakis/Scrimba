9e596b3ee78e835d43d9776ba51af4f815fae594
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-23 19:24:59 -0700

    commit

--- m11-solo-digital-business-card/src/components/Main.js
+++ m11-solo-digital-business-card/src/components/Main.js
@@ -6,17 +6,17 @@ export default function Main() {
             <div className="intro">
                 <h2>Farrah Mamanakis</h2>
                 <h5>Frontend Developer</h5>
-                <a href=''>famanakis.website</a>
+                <a href='famanakis@gmail.com'>famanakis.website</a>
             </div>
             <div>
-                <a className="btn email">
+                <a href="famanakis@gmail.com" className="btn email">
                     <i className="fa-solid fa-envelope"></i>Email</a>
-                <a className="btn linkedIn">
+                <a href="https://www.linkedin.com/in/fmamanakis/" className="btn linkedIn">
                     <i className="fa-brands fa-linkedin"></i>LinkedIn</a>
             </div>
             <div>
                 <h4>About</h4>
-                <p>I am a frontend developer inerested in making attractive websites with pleasing UI design. I try to keep up with best practices and cient security needs. I enjoy learning new techology as it emerges in the ever-evolving tech/dev world. </p>
+                <p>I am a frontend developer interested in making attractive websites with pleasing UI design. I try to keep up with best practices and cient security needs. I enjoy learning new techology as it emerges in the ever-evolving tech/dev world. </p>
                 <h4>Strengths</h4>
                 <p>Attention to Detail. Knowledge of Agile/Scrum. Working with ASD. College Minor in Business Management. Accounts Management Experience.</p>
                 <h4>Interests</h4>

f9849a93f48963bbb8ff885908d070f1ac1eeabc
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-24 19:00:54 -0700

    commit

--- m09-solo-color-scheme-generator/index.js
+++ m09-solo-color-scheme-generator/index.js
@@ -2,45 +2,59 @@
 const color = document.getElementById("color-picker")
 const scheme = document.getElementById("color-schemes")
 const btnGetColor = document.getElementById("btn-get")
+const schemeType = document.getElementById("scheme-type")
 const colorSection = document.getElementById("color-section")
 const hexValues = document.getElementById("hex-values")
+const copyModal = document.getElementById("modal-hex-copied")
 
-
-
-// get new color scheme button action
-btnGetColor.addEventListener('click', (e)=> {
-    e.preventDefault
-    colorSection.innerHTML = ""
-    hexValues.innerHTML = ""
-    renderColors()
-    })
-
-// function to render color and hex schemes to page  
-function renderColors() {
-    const currentColor = color.value.slice(1)
-    const currentScheme = scheme.value.toLowerCase() 
-       
-    fetch (`https://www.thecolorapi.com/scheme?hex=${currentColor}&mode=${currentScheme}&count=5`)
-        .then(res=> res.json())
-        .then(data=> {
-            const colorsArr = data.colors      
-            const colorScheme = colorsArr.map((i)=> {  
-                colorSection.innerHTML += `<div class="color-section" style="background:${i.hex.value}"><span class="space">${i.name.value}</span></div>`
-                hexValues.innerHTML += `<button class="hexText" onclick="SelfCopy('${i.hex.value}')" id="${i.hex.value}"><span>${i.hex.value}</span></button>`
-                })                  
-        }) 
-}
-
-// function to generate random seed color on page load
-function randomColor() {
-    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    return randomColor;
-}
+//Variables
+//color picker gets random color on page load
 color.value = randomColor()
 
+//Functions
+//fetch and render colors from color picker and selection
+async function renderColors() {
+    const currentColor = color.value.slice(1)
+    let currentScheme = scheme.value.toLowerCase() 
+    const res = await fetch (`https://www.thecolorapi.com/scheme?hex=${currentColor}&mode=${currentScheme}&count=5`)
+    const data = await res.json()
+    const colorsArr = data.colors    
+    colorScheme = colorsArr.map((color)=> {  
+        colorSection.innerHTML += `<div class="color-section" style="background:${color.hex.value}">
+                                        <button class="color-text" onclick="SelfCopy('${color.hex.value}')">${color.name.value}</button>
+                                    </div>`
+        hexValues.innerHTML += `<button type="button" id="${color.hex.value}" class="hexText" onclick="SelfCopy('${color.hex.value}')">
+                                    <span>${color.hex.value}</span>
+                                </button>`
+    })                  
+}
 
-// function to copy text to Clipboard
-function SelfCopy(hexColor) {
+//generate random seed color on page load
+function randomColor() {
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+
+//copy text to Clipboard
+async function SelfCopy(hexColor) {
+    copyModal.textContent = hexColor + " copied"
+    copyModal.style.display = 'flex'
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+        try {
+            await navigator.clipboard.writeText(hexColor)
+            copyModal.textContent = hexColor + " copied"
+            copyModal.style.display = 'flex'
+            setTimeout (()=>(copyModal.style.display = 'none'), 1500)
+        } catch (error) {
+            deprecatedCopyToClipboard(hexColor)
+        }
+        } else { //if clipboard API not supported use deprecated CopyToClipboard
+        deprecatedCopyToClipboard(hexColor)
+    }
+}
+
+//deprecated version to work on Scrimba
+function deprecatedCopyToClipboard(hexColor) {
     const area = document.createElement('textarea')
     document.body.appendChild(area)
     area.value = hexColor
@@ -50,18 +64,19 @@ function SelfCopy(hexColor) {
     alert("Copied " + hexColor + " to Clipboard")
 }
 
+//Event Listeners
+btnGetColor.addEventListener('click', (e)=> {
+    e.preventDefault
+    schemeType.textContent = scheme.value.charAt(0).toUpperCase() + scheme.value.slice(1)  
+    colorSection.innerHTML = ""
+    hexValues.innerHTML = ""
+    renderColors()
+    })
 
-// function to copy using navigator -- would need to run first and then change name of textarea function to deprecatedCopyToClipboard
-//function SelfCopy(hexColor) {
-//    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
-//        navigator.clipboard.writeText(hexColor)
-//            .then(() => {
-//                alert("Copied " + hexColor + " to Clipboard")
-//            }, error => { deprecatedCopyToClipboard(hexColor) })
-//    } else { // Clipboard API not supported
-//        deprecatedCopyToClipboard(hexColor)
-//    }
-//}
-
+color.addEventListener('click', () => {
+    colorSection.innerHTML = ""
+    hexValues.innerHTML = ""
+    schemeType.textContent = ""  
+})
 
 

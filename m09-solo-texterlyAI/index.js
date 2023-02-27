86e430ff6c5b8c9c0883f98972df047b4e41f9bd
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-26 21:46:29 -0700

    commit

--- m09-solo-texterlyAI/index.js
+++ m09-solo-texterlyAI/index.js
@@ -3,15 +3,25 @@ const textInput = document.getElementById('text-section')
 const btnProcess = document.getElementById('btn-process')
 const charEl = document.getElementById('char-text')
 const wordEl = document.getElementById('words-text')
+const spinner = document.getElementById('spinner')
+const copied = document.getElementById('copied')
 
 //Variables
 charEl.textContent = 0
 wordEl.textContent = 0
 
 //Functions
+function beginProcess() {  
+    if(textInput.value.length > 0){
+        spinner.classList.remove("hide")
+        setTimeout(processText, 2500)
+    }
+}
+
 function processText() {
     const input = textInput.value
-    getOpenAI(input)
+    spinner.classList.add("hide")
+    // getOpenAI(input)
     charCount(input)
     wordCount(input)
 }
@@ -27,39 +37,71 @@ function wordCount(text) {
     wordEl.textContent = words
 }
 
+function copyAndClear() {
+    if(textInput.value.length > 0) {
+        SelfCopy(textInput.value)
+        setTimeout(()=>(textInput.value = '', 
+        textInput.placeholder = 'Paste your text here.'), 3000)
+    }
+}
+
+async function SelfCopy(text) {
+    copied.classList.remove('hide')
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+        try {
+            await navigator.clipboard.writeText(text)
+            copied.classList.remove('hide')
+            setTimeout (()=>(copied.classList.add('hide')), 2500)
+        } catch (error) {
+            deprecatedCopyToClipboard(text)
+        }
+        } else { //if clipboard API not supported use deprecated CopyToClipboard
+        deprecatedCopyToClipboard(text)
+    }
+}
+//deprecated version to work on Scrimba
+function deprecatedCopyToClipboard(text) {
+    const area = document.createElement('textarea')
+    document.body.appendChild(area)
+    area.value = text
+    area.select()
+    document.execCommand('copy')
+    document.body.removeChild(area)
+}
+
 //Event Listeners
-btnProcess.addEventListener('click', processText)
-textInput.addEventListener('click', ()=> textInput.value = '')
+btnProcess.addEventListener('click', beginProcess)
+textInput.addEventListener('click', copyAndClear)
 
 //Example of grammar correct
-// my API key is in my gmail
-function getOpenAI(userInput) {
-    const API_ENDPOINT = 'https://api.openai.com/v1/edits';
-    const API_KEY = //my API key goes here//; 
+// my API key is in my email
+// function getOpenAI(userInput) {
+//     const API_ENDPOINT = 'https://api.openai.com/v1/edits'
+//     const API_KEY = //my api key goes here 
     
-    const data = {
-    model: 'text-davinci-edit-001',
-    input: userInput,
-    instruction: 'Fix the spelling mistakes',
-    temperature: .2,
-    };
+//     const data = {
+//     model: 'text-davinci-edit-001',
+//     input: userInput,
+//     instruction: 'Fix the spelling mistakes',
+//     temperature: .2,
+//     };
 
-    const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`,
-    },
-    body: JSON.stringify(data),
-    };
+//     const options = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${API_KEY}`,
+//     },
+//     body: JSON.stringify(data),
+//     };
 
-    fetch(API_ENDPOINT, options)
-    .then(response => response.json())
-    .then(data => {
-        const responseAI = data.choices[0].text.trim()
-        console.log(responseAI)
-        textInput.value = textInput.value + '\n' + responseAI
-    })
-    .catch(error => console.error(error));
-}
+//     fetch(API_ENDPOINT, options)
+//     .then(response => response.json())
+//     .then(data => {
+//         const responseAI = data.choices[0].text.trim()
+//         console.log(responseAI)
+//         textInput.value = responseAI
+//     })
+//     .catch(error => console.error(error))
+// }
 

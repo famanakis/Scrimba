d2bd40e00ceb75dbef6c798b798d1e8edfa756b7
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-27 11:53:33 -0700

    commit

--- m11-solo-travel-journal/src/components/Main.js
+++ m11-solo-travel-journal/src/components/Main.js
@@ -2,17 +2,18 @@ import React from "react"
 
 
 export default function Main(props) {
+    const {imageUrl, location, googleMapsUrl, title, startDate, endDate, description} = props
     return (
         <main className="card-container">
             <div className="card">
                 <div>
-                    <img className="card-img" alt="" src={props.imageUrl}/>
+                    <img className="card-img" alt="" src={imageUrl}/>
                 </div>
                 <div>
-                    <p><img src={require ("../images/map-pin-icon.png")} alt="map pin icon" className="map-pin"/> <span className="uppercase">{props.location}</span><a href={props.googleMapsUrl} target="_blank">View on Google Maps</a></p>
-                    <h2>{props.title}</h2>
-                    <p className="card-dates">{props.startDate} - {props.endDate}</p>
-                    <p>{props.description}</p>
+                    <p><img src={require ("../images/map-pin-icon.png")} alt="map pin icon" className="map-pin"/> <span className="uppercase">{location}</span><a href={googleMapsUrl} target="_blank">View on Google Maps</a></p>
+                    <h2>{title}</h2>
+                    <p className="card-dates">{startDate} - {endDate}</p>
+                    <p>{description}</p>
                 </div>
             </div>
         </main>

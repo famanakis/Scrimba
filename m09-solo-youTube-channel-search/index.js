8b575becae5697ef3eac41b7b423d122dda7f4b8
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-17 22:55:12 -0600

    Initial commit

---
+++ m09-solo-youTube-channel-search/index.js
@@ -0,0 +1,12 @@
+// import dotenv from './node_modules/dotenv'
+
+// require('dotenv').config()
+// console.log(process.env)
+
+const apiKey = "Your API key"
+const endpoint = "https://www.googleapis.com/youtube/v3/search"
+const channelId = "Replace with channel ID"
+
+const myApiKey = process.env.API_KEY
+
+console.log(myApiKey)

13a1af02a9ccbb0faf37b55932f7e1bf70a414b4
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-19 15:15:03 -0600

    commit

--- m09-solo-youTube-channel-search/index.js
+++ m09-solo-youTube-channel-search/index.js
@@ -70,3 +70,37 @@ function renderCards() {
 
 //Event Listeners
 btnSearch.addEventListener('click', searchMovies)
+
+
+//Help Page youTube API
+
+//   /**
+//    * Sample JavaScript code for youtube.search.list
+//    * See instructions for running APIs Explorer code samples locally:
+//    * https://developers.google.com/explorer-help/code-samples#javascript
+//    */
+
+//   function loadClient() {
+//     gapi.client.setApiKey("YOUR_API_KEY");
+//     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+//         .then(function() { console.log("GAPI client loaded for API"); },
+//               function(err) { console.error("Error loading GAPI client for API", err); });
+//   }
+//   // Make sure the client is loaded before calling this method.
+//   function execute() {
+//     return gapi.client.youtube.search.list({
+//       "part": [
+//         "snippet"
+//       ],
+//       "channelId": "name here",
+    //   "maxResults": 6,
+    //   "q": "surfing"
+//     })
+//         .then(function(response) {
+//                 // Handle the results here (response.result has the parsed body).
+//                 console.log("Response", response);
+//               },
+//               function(err) { console.error("Execute error", err); });
+//   }
+//   gapi.load("client");
+

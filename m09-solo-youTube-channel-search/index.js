27f09e7e967b67e9b1f08c9ff69d686e67659d36
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-20 21:16:10 -0600

    commit

--- m09-solo-youTube-channel-search/index.js
+++ m09-solo-youTube-channel-search/index.js
@@ -3,7 +3,7 @@ const selectEl = document.getElementById('select-channel')
 const searchEl = document.getElementById('input-search')
 const btnSearch = document.getElementById('btn-search')
 const cardsGrid = document.getElementById('cards-grid')
-const myApiKey = "your API KEY Here"
+const myApiKey = ""
 
 //Functions
 async function testSearch() {
@@ -14,19 +14,8 @@ async function testSearch() {
 
     const res = await fetch(endpoint)
     const data = await res.json()
-    try {
-        const cardsArr = data.items.map(item => 
-            `<div id="${item.id.videoId}" class="card">
-            <a href="https://www.youtube.com/watch?v=${item.id.videoId}" alt="go to video">
-                <img src="${item.snippet.thumbnails.medium.url}" alt="${(item.snippet.title).split('|')[0].trim()}" target="_blank">
-            </a>
-            <p>${(item.snippet.title.split('|')[0].trim().length > 30 ? item.snippet.title.split('|')[0].trim().substring(0, 30) + '...' : item.snippet.title.split('|')[0].trim())}</p>
-            <p class="description">${item.snippet.description}</p>
-            <span class="year">${new Date(item.snippet.publishedAt).getFullYear()}</span>
-            </div>`
-        ).join('')
-    
-        cardsGrid.innerHTML = cardsArr
+    try {  
+        cardsGrid.innerHTML = displayGrid(data)
     }
     catch(error) {
         const message = 'No matching results'
@@ -36,6 +25,21 @@ async function testSearch() {
             </div>`
         throw new Error(message)
     }
+}
+
+function displayGrid(data){
+    const cardsArr = data.items.map(item => 
+            `<div id="${item.id.videoId}" class="card">
+            <a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank" alt="go to video">
+                <img src="${item.snippet.thumbnails.medium.url}" alt="${(item.snippet.title).split('|')[0].trim()}">
+            </a>
+            <p>${(item.snippet.title.split('|')[0].trim().length > 30 ? item.snippet.title.split('|')[0].trim().substring(0, 30) + '...' : item.snippet.title.split('|')[0].trim())}</p>
+            <p class="description">${item.snippet.description}</p>
+            <span class="year">${new Date(item.snippet.publishedAt).getFullYear()}</span>
+            </div>`
+        ).join('')
+        
+    return cardsArr  
 }
   
 //Event Listeners

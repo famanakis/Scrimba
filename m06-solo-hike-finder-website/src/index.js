12797a43436f681031ae2fb777e7d6a9b5683d57
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-20 21:33:00 -0600

    commit

--- m06-solo-hike-finder-website/src/index.js
+++ m06-solo-hike-finder-website/src/index.js
@@ -4,7 +4,7 @@ const mobileNavClose = document.getElementById("mobile-nav-close")
 const btnNavMobile = document.getElementById("btn-nav-mobile")
 const mobileNavLink = document.querySelectorAll('.mobile-nav-link')
 
-btnNavMobile.addEventListener('click', () => mobileNavMenu.style.display = "block")
+btnNavMobile.addEventListener('click', () => mobileNavMenu.style.display = "flex")
 mobileNavClose.addEventListener('click', () => mobileNavMenu.style.display = "none")
 
 mobileNavLink.forEach(link =>  

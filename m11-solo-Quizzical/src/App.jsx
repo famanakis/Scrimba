30ff7a2a63c16693fae62bdc4cefa700dbf66f76
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-27 17:28:49 -0600

    commit

--- m11-solo-Quizzical/src/App.jsx
+++ m11-solo-Quizzical/src/App.jsx
@@ -1,14 +1,14 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-        
+        <h1>hello testing</h1>
     </div>
   )
 }

9692f5dfc614671ac7c032384e84bcc14e792c75
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-27 16:45:57 -0600

    commit

--- m09-solo-movie-watchlist/scripts/addRemoveMovie.js
+++ m09-solo-movie-watchlist/scripts/addRemoveMovie.js
@@ -5,11 +5,11 @@ export function addRemoveMovie(e, storedMovies) {
             storedMovies.push(e.target.dataset.imdbid)
             targetEl.classList.add('minus')
             targetEl.classList.remove('plus')  
-            } else {
-                const removeMovie = storedMovies.findIndex(movie => movie === e.target.dataset.imdbid)
-                storedMovies.splice(removeMovie, 1)
-                targetEl.classList.add('plus')
-                targetEl.classList.remove('minus')
+        } else {
+            const removeMovie = storedMovies.findIndex(movie => movie === e.target.dataset.imdbid)
+            storedMovies.splice(removeMovie, 1)
+            targetEl.classList.add('plus')
+            targetEl.classList.remove('minus')
         }
     localStorage.setItem("movieID", JSON.stringify(storedMovies))
     }

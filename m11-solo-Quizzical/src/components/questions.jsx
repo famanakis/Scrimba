6b0627eb91a6f2fe773188d2c0a29d53033498a5
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-28 22:51:24 -0600

    commit

--- m11-solo-Quizzical/src/components/questions.jsx
+++ m11-solo-Quizzical/src/components/questions.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Questions(props) {
     return (
-        <div className="questions">
+        <div className={`questions ${props.startGame ? 'flex' : 'none'}`}>
             <h2>{props.question}</h2>
             <div className="answers">
-                <button className="btn-answer">{props.opt1}</button>
-                <button className="btn-answer">{props.opt2}</button>
-                <button className="btn-answer">{props.opt3}</button>
-                <button className="btn-answer">{props.opt4}</button>
+                <button>{props.opt1}</button>
+                <button>{props.opt2}</button>
+                <button>{props.opt3}</button>
+                <button>{props.opt4}</button>
             </div>
             <p></p>
         </div>

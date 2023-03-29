c5f45b442c63df607750715738cf8df1dc8e86c0
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-28 21:35:22 -0600

    commit

--- m11-solo-Quizzical/src/components/Start.jsx
+++ m11-solo-Quizzical/src/components/Start.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 function Start() {
+
+    function startGame() {
+        console.log('clicked')
+        document.getElementsByClassName('.start').style.display = "none"
+        document.getElementsByClassName('.questions').style.display = "flex"
+    }
+
     return (
         <div className="start">
             <h1>Quizzical</h1>
             <p>Let's have some fun!</p>
-            <button className="btn-start">Start quiz</button>
+            <button className="btn-start" onClick={startGame}>Start quiz</button>
         </div>
     )
 }

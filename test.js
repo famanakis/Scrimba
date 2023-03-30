32ce7315fdd54990b3b72b5a200ad5eadcf048c9
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-29 22:04:37 -0600

    commit

---
+++ test.js
@@ -0,0 +1,64 @@
+const results = [
+    {
+        "category": "Entertainment: Video Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "In Terraria, what does the Wall of Flesh not drop upon defeat?",
+        "correct_answer": "Picksaw",
+        "incorrect_answers": [
+            "Pwnhammer",
+            "Breaker Blade",
+            "Laser Rifle"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Which game is NOT part of the Science Adventure series by 5pb. and Nitroplus?",
+        "correct_answer": "Occultic; Nine",
+        "incorrect_answers": [
+            "Steins; Gate",
+            "Robotics; Notes",
+            "Chaos; Child"
+        ]
+    },
+    {
+        "category": "Vehicles",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "Which Audi does not use Haldex based all wheel drive system?",
+        "correct_answer": "Audi A8",
+        "incorrect_answers": [
+            "Audi TT",
+            "Audi S3",
+            "Audi A3"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "Toussaint Louverture led a successful slave revolt in which country?",
+        "correct_answer": "Haiti",
+        "incorrect_answers": [
+            "France",
+            "Cuba",
+            "United States"
+        ]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Which of the following dice is not a platonic solid?",
+        "correct_answer": "10-sided die",
+        "incorrect_answers": [
+            "12-sided die",
+            "20-sided die",
+            "8-sided die"
+        ]
+    }
+]
+
+

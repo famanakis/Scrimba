80fc03aefcdc046b1dbf7754880ed8ecb3165b65
famanakis <73799603+famanakis@users.noreply.github.com>
2023-03-31 18:00:43 -0600

    commit

--- m11-solo-Quizzical/src/App.jsx
+++ m11-solo-Quizzical/src/App.jsx
@@ -16,15 +16,18 @@ function App() {
   const [count, setCount] = useState(0)
   const [questions, setQuestions] = useState([])
 
+//useEffect makes call to outside source OpenTriviaDB API
   useEffect(()=> {
     async function getQuestions() {
       const res = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       const data = await res.json()
       let triviaArr = []
       data.results.forEach(item => {
+        //use he to decode the data to avoid HTML entities in the text
         const decodedQuestion = he.decode(item.question)
         const decodedCorrectAnswer = he.decode(item.correct_answer)
         const decodedIncorrectAnswers = item.incorrect_answers.map(answer => he.decode(answer))
+        //push the following object with data into triviaArr
         triviaArr.push({
           id:nanoid(), 
           question:decodedQuestion, 
@@ -33,11 +36,14 @@ function App() {
           checked:false
         })
       })
+      //setQuestions value becomes triviaArr
       setQuestions(triviaArr)
     }
+      //call function getQuestions()
       getQuestions()
   }, [count])
 
+  //create triviaElement which is a Question component with props to pass API data from App to the component
   const triviaElement = questions ? questions.map(item => {
     return (
       <Question 

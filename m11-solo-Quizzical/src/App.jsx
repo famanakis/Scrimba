7e3b5a94964b8a5302b2b42ec9a3a34c7217b84f
famanakis <73799603+famanakis@users.noreply.github.com>
2023-04-03 13:14:46 -0600

    commit

--- m11-solo-Quizzical/src/App.jsx
+++ m11-solo-Quizzical/src/App.jsx
@@ -68,21 +68,21 @@ function App() {
   }
 
   return (
-    <main>
-      <div className={startGame ? 'blob-yellow-small' : 'blob-yellow'}></div>
-      <div className={startGame ? 'blob-blue-small' : 'blob-blue'}></div>
+      <main>
+        <div className={startGame ? 'blob-yellow-small' : 'blob-yellow'}></div>
+        <div className={startGame ? 'blob-blue-small' : 'blob-blue'}></div>
 
-      <Start onStartGame={handleStartGame} startGame={startGame}/>
-      
-      {triviaElement}
+        <Start onStartGame={handleStartGame} startGame={startGame}/>
+        
+        {triviaElement}
 
-      <Footer startGame = {startGame}
-        onStartGame={handleStartGame} 
-        handleCheckAnswers={() => {handleCount(count), setCheckAnswers(true)}} 
-        checkAnswers={checkAnswers} 
-        count={count}
-      />
-    </main>
+        <Footer startGame = {startGame}
+          onStartGame={handleStartGame} 
+          handleCheckAnswers={() => {handleCount(count), setCheckAnswers(true)}} 
+          checkAnswers={checkAnswers} 
+          count={count}
+        />
+      </main>
   )
 }
 
--- m11-solo-Quizzical/src/components/Footer.jsx
+++ m11-solo-Quizzical/src/components/Footer.jsx
@@ -4,9 +4,12 @@ function Footer(props) {
     const {startGame, checkAnswers, onStartGame, count, handleCheckAnswers} = props
     
     return (
-        <div className={`footer ${startGame ? 'flex' : 'none'}`}>
-            <h2 className={`${checkAnswers ? 'flex' : 'none'}`}>You scored {count}/5 correct answers</h2>
-            <button onClick={checkAnswers ? onStartGame : handleCheckAnswers} className="btn-check">{checkAnswers ? 'Play Again' : 'Check Answers'}</button>
+        <div className={`${startGame ? 'flex' : 'none'}`}>
+            <div className="footer">
+                <h2 className={`${checkAnswers ? 'flex' : 'none'}`}>You scored {count}/5 correct answers</h2>
+                <button onClick={checkAnswers ? onStartGame : handleCheckAnswers} 
+                className="btn-check">{checkAnswers ? 'Play Again' : 'Check Answers'}</button>
+            </div>
         </div>
     )
 }
